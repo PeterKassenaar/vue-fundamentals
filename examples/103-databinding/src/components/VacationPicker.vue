eb09803363b4fd0253ccb8bcf7f6c4137025a25d
PeterKassenaar <info@kassenaar.com>
2018-12-17 15:36:20 +0100

    added example on data binding

---
+++ examples/103-databinding/src/components/VacationPicker.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="">
+        <h1>{{ header }}</h1>
+        <ul class="list-group">
+            <li class="list-group-item" v-for="(country, index) in data.countries">
+                <span :id="country.id"
+                      :title="country.details">
+                    {{ index}} - {{country.name}}
+                </span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+	// import the country data
+	import data from '../data/data';
+
+	export default {
+		name: 'VacationPicker',
+		data() {
+			return {
+				// make data available in app. This is shorthand notation for 'data : data'
+				data,
+				header: 'Pick your next vacation'
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
