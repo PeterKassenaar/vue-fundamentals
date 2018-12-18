ee25bb453b2b359cd074b44aaa30d29a681ece06
PeterKassenaar <info@kassenaar.com>
2018-12-18 13:47:18 +0100

    added examples on event binding and computed properties

---
+++ examples/104-eventbinding/src/components/VacationPicker.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="">
+        <h1>{{ header }}</h1>
+        <ul class="list-group">
+            <li @mouseover="logCity(country.name)"
+                class="list-group-item" v-for="country in data.countries" :key="country.id">
+                <span :id="country.id"
+                      :title="country.details">
+                    {{ country.id }} - {{country.name}}
+                </span>
+            </li>
+        </ul>
+        <hr>
+        <h2>Capture click events</h2>
+        <h3>{{ counter }}</h3>
+        <button @click="increase()" class="btn btn-success">Increase ++</button>
+        <button @click="decrease()" class="btn btn-danger">Decrease --</button>
+    </div>
+</template>
+
+<script>
+	// import the country data
+	import data from '../data/data';
+
+	export default {
+		name: 'VacationPicker',
+		data() {
+			return {
+				// make data available in app. This is shorthand notation for 'data : data'
+				data,
+				header: 'Pick your next vacation',
+				counter: 0
+			}
+		},
+		methods: {
+			// long-hand notation:
+			increase() {
+				console.log('The counter is increased: ', this.counter++);
+				this.counter++;
+			},
+			decrease() {
+				console.log('The counter is decreased: ', this.counter--);
+				this.counter--;
+			},
+			logCity(countryName) {
+				console.log({countryName});
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ examples/105-computed-properties/src/components/VacationPicker.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="">
+        <h1>{{ header }}</h1>
+        <div class="row">
+            <div class="col-6">
+                <ul class="list-group">
+                    <li @click="selectCountry(country)"
+                        class="list-group-item" v-for="country in data.countries" :key="country.id">
+                <span :id="country.id"
+                      :title="country.details">
+                    {{ country.id }} - {{country.name}}
+                </span>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-6">
+                <h2>Selected:</h2>
+                <ul class="list-group">
+                    <li class="list-group-item">{{ selectedCountry.id}}</li>
+                    <li class="list-group-item">{{ selectedCountry.name}}</li>
+                    <li class="list-group-item">{{ selectedCountry.capital}}</li>
+                    <li class="list-group-item">{{ selectedCountry.details}}</li>
+                    <li class="list-group-item" v-if="isExpensive">
+                        <span class="badge badge-danger badge-pill">Expensive!</span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+    </div>
+</template>
+
+<script>
+	// import the country data
+	import data from '../data/data';
+
+	export default {
+		name: 'VacationPicker',
+		data() {
+			return {
+				// make data available in app. This is shorthand notation for 'data : data'
+				data,
+				header: 'Pick your next vacation',
+				counter: 0,
+				selectedCountryIndex: 0
+			}
+		},
+		methods: {
+			selectCountry(country) {
+				this.selectedCountryIndex = this.data.countries.indexOf(country);
+			}
+		},
+		computed: {
+			selectedCountry() {
+				return {
+					// longhand notation: annotate every property
+					// id: this.data.countries[this.selectedCountryIndex].id,
+					// name: this.data.countries[this.selectedCountryIndex].name,
+					// capital: this.data.countries[this.selectedCountryIndex].capital,
+					// cost: this.data.countries[this.selectedCountryIndex].cost,
+					// details: this.data.countries[this.selectedCountryIndex].details,
+					// img: this.data.countries[this.selectedCountryIndex].img
+
+					// shorthand notation: use the spread operator (=more elegant & scalable)
+					...this.data.countries[this.selectedCountryIndex]
+				}
+			},
+			isExpensive() {
+				return data.countries[this.selectedCountryIndex].cost > 4000;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
