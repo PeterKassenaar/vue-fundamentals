d73318c70dd1763a41b15a6f010e82bbee380d7c
Peter Kassenaar <info@kassenaar.com>
2019-01-13 10:23:05 +0100

    added examples on props

---
+++ examples/200-props/src/components/CountryDetail.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <h2>{{ country.name }}</h2>
+        <ul class="list-group">
+            <li class="list-group-item">{{ country.id}}</li>
+            <li class="list-group-item">{{ country.name}}</li>
+            <li class="list-group-item">{{ country.capital}}</li>
+            <li class="list-group-item">
+                <img :src="getImgUrl(country.img)"
+                     :alt="country.img"
+                     class="img-fluid">
+            </li>
+            <li class="list-group-item">{{ country.details}}</li>
+            <li class="list-group-item" v-if="isExpensive">
+                <span class="badge badge-danger badge-pill">Expensive!</span>
+            </li>
+            <li class="list-group-item" v-if="isOnSale">
+                <span class="badge badge-warning badge-pill">On Sale!</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: "CountryDetail",
+		props: ['country'],
+		methods: {
+			getImgUrl(img) {
+				console.log(img);
+				return require('../assets/countries/' + img);
+			}
+		},
+		computed: {
+			isExpensive() {
+				return this.country.cost > 4000;
+			},
+			isOnSale() {
+				return this.country.cost < 1000;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ examples/210-props-validation/src/components/VacationPicker.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="">
+        <h1>
+            <img src="../assets/logo.png" alt="vue logo" style="width:80px"/>
+            Vue vacation picker</h1>
+        <div class="row">
+            <div class="col-6">
+                <div class="alert alert-warning text-center">
+                    <h1>{{ country.name}}</h1>
+                    <h2>Capital: {{ country.capital}}
+                        <span v-if="isOnSale" class="badge badge-light">Sale!</span>
+                        <span v-if="isExpensive" class="badge badge-danger">(expensive)</span>
+                    </h2>
+                </div>
+                <button @click="prevCountry()" class="btn btn-success"> << Back</button>
+                <button @click="nextCountry()" class="btn btn-success">Forward >></button>
+                <button @click="showDetails = !showDetails" class="btn btn-primary">
+                    <span v-if="showDetails">Hide details</span>
+                    <span v-if="!showDetails">Show details</span>
+                </button>
+            </div>
+            <div class="col-6">
+                <CountryDetail v-if="showDetails"
+                               :name="country.name"
+                               :country="country" />
+            </div>
+        </div>
+        <hr>
+        <div class="small text-center">Copyright (C) 2019 </div>
+    </div>
+</template>
+
+<script>
+	// import the country data
+	import data from '../data/data';
+	import CountryDetail from "./CountryDetail";
+
+	export default {
+		name: 'VacationPicker',
+		components: { CountryDetail},
+		//**************** Data
+		data() {
+			return {
+				data,
+				showDetails: false,
+				currentCountryIndex: 0
+			}
+		},
+        //**************** Methods
+		methods: {
+			prevCountry: function () {
+				this.currentCountryIndex--;
+				if (this.currentCountryIndex <= 0) {
+					this.currentCountryIndex = this.data.countries.length - 1;
+				}
+			},
+			nextCountry: function () {
+				this.currentCountryIndex++;
+				if (this.currentCountryIndex > this.data.countries.length - 1) {
+					this.currentCountryIndex = 0;
+				}
+			}
+		},
+        //**************** Computed properties
+		computed: {
+			country() {
+				return {
+					...this.data.countries[this.currentCountryIndex]
+				}
+			},
+			isExpensive() {
+				return this.country.cost > 4000;
+			},
+			isOnSale() {
+				return this.country.cost < 1000;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
