c63106c60ce2a7e06642f5066dfc8213cb76fd26
PeterKassenaar <info@kassenaar.com>
2019-01-14 14:31:41 +0100

    example on slots added

---
+++ examples/230-slots/src/shared/CollapsibleSection.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="alert alert-info" style="cursor: pointer">
+            <span v-if="open" @click="open = !open">&#x25B2; Collapse</span>
+            <span v-if="!open" @click="open = !open">&#x25BC; Expand</span>
+        </div>
+        <!--Injected content here-->
+        <slot v-if="open"></slot>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: "CollapsibleSection",
+		data() {
+			return {
+				open: false
+			}
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
