f1c9570d9176ec1b33d6744942145057b79b290f
Peter Kassenaar <info@kassenaar.com>
2019-03-25 13:58:48 +0100

    updated 103-databinding with new workshop

--- examples/103-databinding/src/components/VacationPicker.vue
+++ examples/103-databinding/src/components/VacationPicker.vue
@@ -2,27 +2,30 @@
     <div class="">
         <h1>{{ header }}</h1>
         <ul class="list-group">
-            <li class="list-group-item" v-for="(country, index) in data.countries">
+            <li class="list-group-item" v-for="country in data.countries">
                 <span :id="country.id"
-                      :title="country.details">
-                    {{ index}} - {{country.name}}
+                     :title="country.details">
+                    {{country.name}}
                 </span>
             </li>
         </ul>
+        <!--**********************-->
+        <!--Workshop: Add your CustomerData here-->
+        <!--**********************-->
     </div>
 </template>
 
 <script>
-	// import the country data
-	import data from '../data/data';
+	// import the country countryData
+	import countryData from '../data/CountryData';
 
 	export default {
 		name: 'VacationPicker',
 		data() {
 			return {
-				// make data available in app. This is shorthand notation for 'data : data'
-				data,
-				header: 'Pick your next vacation'
+				// make countryData available in app. This is shorthand notation for 'countryData : countryData'
+				data: countryData,
+				header: 'Pick your next vacation',
 			}
 		}
 	}
