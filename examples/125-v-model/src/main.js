9faef7ca12648f64fc4db28965dd83c0811b2141
Peter Kassenaar <info@kassenaar.com>
2019-04-24 07:58:55 +0200

    Added v-model example

---
+++ examples/125-v-model/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false;
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
