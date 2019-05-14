452b9b0d2bae301fc4addcc45025ab341d32e3d2
Peter Kassenaar <info@kassenaar.com>
2019-05-14 10:19:32 +0200

    Added example on currency filter

--- examples/170-filters/src/filters/filters.js
+++ examples/170-filters/src/filters/filters.js
@@ -3,7 +3,23 @@ import Vue from 'vue';
 
 // Defining a global filter, before creating the Vue instance
 // This assigns a leading zero if id < 10
-Vue.filter('formatId', function (value) {
-	if(!value)return;
+Vue.filter('formatId', value => {
+	if (!value) return;
 	return value >= 10 ? value : '0' + value
+});
+
+// Defining filter parameters
+Vue.filter('currency', (value, locale, symbol) => {
+	if (!value) return;
+	return Intl.NumberFormat(locale, {
+		style: 'currency',
+		currency: symbol
+	}).format(value)
+});
+
+// Invalid: it is forbidden to add HTML to values.
+// In the UI the HTML-tags will be escaped
+Vue.filter('mailto', value => {
+	if (!value) return;
+	return `<a href="mailto:${value}">${value}</a>`;
 });
