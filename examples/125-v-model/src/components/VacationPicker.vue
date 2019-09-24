ecc52ac7211ee9f16ed32a193bd69343e24662f1
Peter Kassenaar <info@kassenaar.com>
2019-09-24 08:03:36 +0200

    Updated example on 125-v-model

--- examples/125-v-model/src/components/VacationPicker.vue
+++ examples/125-v-model/src/components/VacationPicker.vue
@@ -15,33 +15,16 @@
                 </span>
                     </li>
                 </ul>
-            </div>
-            <div class="col-6">
-                <h2>Selected:</h2>
-                <ul class="list-group">
-                    <li class="list-group-item">{{ selectedCountry.id}}</li>
-                    <li class="list-group-item">{{ selectedCountry.name}}</li>
-                    <li class="list-group-item">{{ selectedCountry.capital}}</li>
-                    <li class="list-group-item">{{ selectedCountry.details}}</li>
-                    <li class="list-group-item">
-                        <img :src="getImgUrl(selectedCountry.img)"
-                             :alt="selectedCountry.img"
-                             class="img-fluid" >
-                    </li>
-                    <li class="list-group-item" v-if="isExpensive">
-                        <span class="badge badge-danger badge-pill">
-                            Expensive!
-                        </span>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
+                <!--                *********************-->
+                <!--                Creating a list of new countries via v-model-->
+                <!--                *********************-->
                 <hr>
-                <h2>New Countries</h2>
-                <input type="text" class="form-control-lg"
+                <h2><label for="newCountry">New Countries</label></h2>
+                <input type="text"
+                       id="newCountry"
+                       class="form-control-lg"
                        v-model="newCountry"
+                       placeholder="New Country"
                        @keyup.enter="addCountry()"
                 >
                 <button @click="addCountry()" class="btn btn-info">
@@ -55,60 +38,84 @@
                 </ul>
 
             </div>
+            <div class="col-6">
+                <h2>Selected:</h2>
+                <ul class="list-group">
+                    <li class="list-group-item">{{ selectedCountry.id}}</li>
+                    <li class="list-group-item">{{ selectedCountry.name}}</li>
+                    <li class="list-group-item">{{ selectedCountry.capital}}</li>
+                    <li class="list-group-item">{{ selectedCountry.details}}</li>
+                    <li class="list-group-item">
+                        <img :src="getImgUrl(selectedCountry.img)"
+                             :alt="selectedCountry.img"
+                             class="img-fluid">
+                    </li>
+                    <li class="list-group-item" v-if="isExpensive">
+                        <span class="badge badge-danger badge-pill">
+                            Expensive!
+                        </span>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-    import data from '../data/data';
-    import mixins from '../mixins/mixins';
+	import data from '../data/data';
+	import mixins from '../mixins/mixins';
 
-    export default {
-        name: "VacationPicker",
-        data() {
-            return {
-                data,
-                title: 'Vue Vacation Picker',
-                selectedCountryIndex: 0,
-                newCountry: '',
-                newCountries:[]
-            }
-        },
-        // Code, mixed in to this component
-        mixins:[mixins],
-        // lifecycle hook
-        created(){
-            console.log('Vacation Picker is created...');
-        },
-        methods: {
-            selectCountry(index) {
-                this.selectedCountryIndex = index;
-            },
-            addCountry(){
-                this.newCountries.push(this.newCountry);
-                this.newCountry='';
-            },
-        },
-        computed: {
-            selectedCountry() {
-                return {
-                    // longhand notation
-                    // id: this.data.countries[this.selectedCountryIndex].id,
-                    // name: this.data.countries[this.selectedCountryIndex].name,
-                    // capital: this.data.countries[this.selectedCountryIndex].capital,
-                    // cost: this.data.countries[this.selectedCountryIndex].cost,
-                    // details: this.data.countries[this.selectedCountryIndex].details,
-                    // img: this.data.countries[this.selectedCountryIndex].img
+	export default {
+		name: "VacationPicker",
+		data() {
+			return {
+				data,
+				title: 'Vue Vacation Picker',
+				selectedCountryIndex: 0,
+				newCountry: '',
+				newCountries: []
+			}
+		},
+		// Code, mixed in to this component
+		mixins: [mixins],
+		// lifecycle hook
+		created() {
+			console.log('Vacation Picker is created...');
+		},
+		methods: {
+			selectCountry(index) {
+				this.selectedCountryIndex = index;
+			},
+			addCountry() {
+				this.newCountries.push(this.newCountry);
+				this.newCountry = '';
+			},
+		},
+		computed: {
+			selectedCountry() {
+				return {
+					// longhand notation
+					// id: this.data.countries[this.selectedCountryIndex].id,
+					// name: this.data.countries[this.selectedCountryIndex].name,
+					// capital: this.data.countries[this.selectedCountryIndex].capital,
+					// cost: this.data.countries[this.selectedCountryIndex].cost,
+					// details: this.data.countries[this.selectedCountryIndex].details,
+					// img: this.data.countries[this.selectedCountryIndex].img
 
-                    // shorthand notation
-                    ...this.data.countries[this.selectedCountryIndex]
-                }
-            },
-            isExpensive(){
-                return this.data.countries[this.selectedCountryIndex].cost > 4000;
-            }
-        }
-    }
+					// shorthand notation
+					...this.data.countries[this.selectedCountryIndex]
+				}
+			},
+			isExpensive() {
+				return this.data.countries[this.selectedCountryIndex].cost > 4000;
+			}
+		}
+	}
 </script>
 
 <style scoped>
