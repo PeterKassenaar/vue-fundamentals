854dcab62ba209d8d24f7c74d6828bb402c817e7
Peter Kassenaar <info@kassenaar.com>
2019-09-25 10:21:46 +0200

    Updated 300-routing-basics example

--- examples/300-routing-basics/src/components/AddCountry.vue
+++ examples/300-routing-basics/src/components/AddCountry.vue
@@ -2,19 +2,13 @@
     <div>
         <h1>Add a Country</h1>
         <h2>// TODO... </h2>
-        <button @click="goHome()">Home</button>
+
     </div>
 </template>
 
 <script>
 	export default {
 		name: "AddCountry",
-        methods:{
-			goHome(){
-				this.$router.push('/')
-            }
-
-        }
 	}
 </script>
 
