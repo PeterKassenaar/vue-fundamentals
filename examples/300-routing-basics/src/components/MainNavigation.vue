961108a66d2fd7f921ce17b723b121a898287a57
Peter Kassenaar <info@kassenaar.com>
2019-10-22 19:29:20 +0200

    Updated routing example

--- examples/300-routing-basics/src/components/MainNavigation.vue
+++ examples/300-routing-basics/src/components/MainNavigation.vue
@@ -3,7 +3,7 @@
         <div class="col">
             <!--Main navigation - Bootstrap Navbar, see https://getbootstrap.com/docs/4.0/components/navbar/ for details -->
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <router-link class="navbar-brand transparant" exact :to="homeRoute">
+                <router-link class="navbar-brand" exact :to="{name: 'home'}">
                     <img src="../assets/logo.png" alt="Vue Logo">
                 </router-link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
@@ -15,13 +15,13 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
-                            <router-link to="/" class="nav-link" exact >Home</router-link>
+                            <router-link to="/" class="nav-link" >Home</router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link to="add" class="nav-link" exact>Add Country</router-link>
+                            <router-link to="add" class="nav-link">Add Country</router-link>
                         </li>
                         <li class="nav-item">
-                            <router-link to="update" class="nav-link" exact>Update Country</router-link>
+                            <router-link to="update" class="nav-link">Update Country</router-link>
                         </li>
                     </ul>
                 </div>
@@ -48,11 +48,11 @@
         height: 40px;
     }
     /*Automatic assignment of this class by Vue, based on router state*/
-    .router-link-active{
-        background-color: rebeccapurple;
-        color: white;
-    }
-    .transparant{
-        background-color: transparent !important;
-    }
+    /*.router-link-active{*/
+    /*    background-color: #dddddd;*/
+    /*    color: white;*/
+    /*}*/
+    /*.transparant{*/
+    /*    background-color: transparent !important;*/
+    /*}*/
 </style>
