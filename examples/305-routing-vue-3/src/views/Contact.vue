26396e25f268d45b054706a3d6c27e0818685b13
Peter Kassenaar <info@kassenaar.com>
2020-11-10 13:13:06 +0100

    Added 305-routing-vue-3 example

---
+++ examples/305-routing-vue-3/src/views/Contact.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2>This is a contact page</h2>
+</template>
+
+<script>
+export default {
+  name: "Contact"
+}
+</script>
---
+++ examples/305-routing-vue-3/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h2>Hello homepage</h2>
+    <h3>Routing in Vue 3.x</h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+}
+</script>
