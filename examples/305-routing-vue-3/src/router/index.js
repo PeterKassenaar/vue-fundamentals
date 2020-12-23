eacb7e6d437f719925face9cb91bff1977d18c41
Peter Kassenaar <info@kassenaar.com>
2020-12-23 10:58:48 +0100

    Updated 404-route in Vue 3

--- examples/305-routing-vue-3/src/router/index.js
+++ examples/305-routing-vue-3/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Contact from "@/views/Contact";
+import FileNotFound from "../components/FileNotFound";
 
 const routes = [
   {
@@ -20,7 +21,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/:catchAll(.*)*',
+    component: FileNotFound
+  },
 ]
 
 const router = createRouter({
