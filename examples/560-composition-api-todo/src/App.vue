cde8fa740b136f5205d0d3c2a2e78355c5bdf69c
PeterKassenaar <info@kassenaar.com>
2021-05-04 14:43:04 +0200

    Updated TodoList Composition example

--- examples/560-composition-api-todo/src/App.vue
+++ examples/560-composition-api-todo/src/App.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="container">
-        <TodoList/>
-        <hr>
-<!--        <TodoListComposition/>-->
-    </div>
+  <div class="container">
+    <!--      1. TodoList, WITHOUT extracting the composition to external file(s)-->
+     <TodoList/>
+    <hr>
+    <!--      2. TodoList, WITH extracting the composition to external file(s)-->
+<!--    <TodoListComposition/>-->
+  </div>
 </template>
 
 <script>
-    // import TodoListComposition from "./components/TodoListComposition";
-    import TodoList from "./components/TodoList";
+// import TodoListComposition from "./components/TodoListComposition";
+import TodoList from "./components/TodoList";
 
-    export default {
-        name: 'App',
-        components: {
-            TodoList,
-            // TodoListComposition,
-        }
-    }
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+    // TodoListComposition,
+  }
+}
 </script>
