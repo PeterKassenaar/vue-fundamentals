8d56332933fa8296f886cec5b48b76d2174c0a1f
Peter Kassenaar <info@kassenaar.com>
2023-02-27 19:12:11 +0100

    updated TextBox examples

--- examples/200-props/src/components/workshop/TextboxChild.vue
+++ examples/200-props/src/components/workshop/TextboxChild.vue
@@ -1,29 +1,29 @@
 <template>
-<div>
+  <div>
     <hr>
     <h3>Coming from the parent component ==></h3>
     <h3>{{ message }}</h3>
     <h3>This component is updated {{ counter }} time<span v-if="counter !== 1">s</span>
     </h3>
-</div>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: "TextboxChild",
-        props: ['message'],
-        data(){
-			return{
-				counter: 1
-            }
-        },
-        // lifecycle hook, called before the message is updated from the parent.
-        // Try what happens if you use the 'updated()' lifecycle hook here!
-        beforeUpdate() {
-			console.log('beforeupdate() called...' + this.message);
-			this.counter++;
-		}
-	}
+export default {
+  name: "TextboxChild",
+  props: ['message'],
+  data() {
+    return {
+      counter: 1
+    }
+  },
+  // lifecycle hook, called before the message is updated from the parent.
+  // Try what happens if you use the 'updated()' lifecycle hook here!
+  beforeUpdate() {
+    console.log('beforeupdate() called...' + this.message);
+    this.counter++;
+  }
+}
 </script>
 
 <style scoped>
