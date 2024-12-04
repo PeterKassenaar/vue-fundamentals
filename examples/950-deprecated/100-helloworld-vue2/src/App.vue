ad0a0bf33f27b07a892ed21788684be59a1a16ea
PeterKassenaar <info@kassenaar.com>
2024-12-04 13:41:20 +0100

    feat: update Vue 3 examples

--- examples/950-deprecated/100-helloworld-vue2/src/App.vue
+++ examples/950-deprecated/100-helloworld-vue2/src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue 3 App" name="Peter"/>
+  <div id="app">
+    <img alt="Vue logo" src="assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" name="Peter"/>
+  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
     HelloWorld
   }
@@ -16,7 +18,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
