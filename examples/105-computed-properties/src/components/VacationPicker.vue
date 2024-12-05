4bcd991bd9318ccefb923a8803598bb67d3069cb
PeterKassenaar <info@kassenaar.com>
2024-12-05 10:29:22 +0100

    feat: update example 105-computed-properties

--- examples/105-computed-properties/src/components/VacationPicker.vue
+++ examples/105-computed-properties/src/components/VacationPicker.vue
@@ -1,88 +1,62 @@
 <template>
-    <div class="">
-        <h1>{{ header }}</h1>
-        <div class="row">
-            <div class="col-6">
-                <ul class="list-group">
-                    <li @click="selectCountry(country)"
-                        class="list-group-item"
-                        v-for="country in data.countries"
-                        :key="country.id">
+  <h1>{{ header }}</h1>
+  <div class="row">
+    <div class="col-6">
+      <ul class="list-group">
+        <li @click="selectCountry(country.id)"
+            class="list-group-item"
+            v-for="country in data.countries"
+            :key="country.id">
                 <span :id="country.id"
                       :title="country.details">
-                    {{ country.id }} - {{country.name}}
+                    {{ country.id }} - {{ country.name }}
                 </span>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-6">
-                <h2>Selected:</h2>
-                <ul class="list-group">
-                    <li class="list-group-item">{{ selectedCountry.id}}</li>
-                    <li class="list-group-item">{{ selectedCountry.name}}</li>
-                    <li class="list-group-item">{{ selectedCountry.capital}}</li>
-                    <li class="list-group-item">{{ selectedCountry.details}}</li>
-                    <li class="list-group-item" v-if="isExpensive">
-                        <span class="badge badge-danger badge-pill">Expensive!</span>
-                    </li>
-                    <!--*******************-->
-                    <!--Workshop: Show a badge if a destination is On Sale -->
-                    <!--i.e. costs less than 1000-->
-                    <!--*******************-->
-                </ul>
-            </div>
-        </div>
-        <hr>
+        </li>
+      </ul>
     </div>
+    <div class="col-6" v-if="currentCountry">
+      <h2>Selected:</h2>
+      <ul class="list-group">
+        <li class="list-group-item">{{ currentCountry.id }}</li>
+        <li class="list-group-item">{{ currentCountry.name }}</li>
+        <li class="list-group-item">{{ currentCountry.capital }}</li>
+        <li class="list-group-item">{{ currentCountry.details }}</li>
+        <li class="list-group-item" v-if="isExpensive">
+          <span class="badge bg-danger rounded-pill">Expensive!</span>
+        </li>
+        <!--*******************-->
+        <!--Workshop: Show a badge if a destination is On Sale -->
+        <!--i.e. costs less than 1000-->
+        <!--*******************-->
+      </ul>
+    </div>
+  </div>
 </template>
 
-<script>
-	// import the country data
-	import data from '../data/data';
+<script setup>
+import {computed, ref} from "vue";
+// import the country countryData
+import countryData from '@/data/CountryData';
 
-	export default {
-		name: 'VacationPicker',
-		data() {
-			return {
-				// make data available in app. This is shorthand notation for 'data : data'
-				data,
-				header: 'Pick your next vacation',
-				counter: 0,
-				selectedCountryIndex: 0
-			}
-		},
-		methods: {
-			selectCountry(country) {
-				this.selectedCountryIndex = this.data.countries.indexOf(country);
-			}
-		},
-		computed: {
-			selectedCountry() {
-				console.log('called selectedCountry computed property...');
-				return {
-					// longhand notation: annotate every property
-					id: this.data.countries[this.selectedCountryIndex].id,
-					name: this.data.countries[this.selectedCountryIndex].name,
-					capital: this.data.countries[this.selectedCountryIndex].capital,
-					cost: this.data.countries[this.selectedCountryIndex].cost,
-					details: this.data.countries[this.selectedCountryIndex].details,
-					img: this.data.countries[this.selectedCountryIndex].img
+// create variables
+const header = 'Pick your next vacation';
+const data = countryData;
+const currentCountryId = ref(0);
 
-					// shorthand notation: use the spread operator
-                    // (=more elegant & scalable)
+// Automatically calculate current country, based on Id
+const currentCountry = computed(()=>{
+  return data.countries.find(c => c.id === currentCountryId.value)
+})
 
-					// ...this.data.countries[this.selectedCountryIndex]
-				}
-			},
-			isExpensive() {
-				return data.countries[this.selectedCountryIndex].cost > 4000;
-			}
-			// Workshop: create a computed property for the isOnSale property of this component
+// Automatically calculate if a destination is expensive
+const isExpensive = computed(() => {
+  return currentCountry.value.cost > 4000;
+});
+// Workshop: create a computed property for the isOnSale property of this component
 
-		}
-	}
+
+// Update the Id when the user clicks a country
+const selectCountry = id => {
+  currentCountryId.value = id;
+}
 </script>
-
-<style scoped>
-
-</style>
