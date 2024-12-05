0a0a5cf6465f3c9c79826974a1ab138e97a414b3
PeterKassenaar <info@kassenaar.com>
2024-12-05 14:43:22 +0100

    feat: update example 126-v-model-selectionlist

--- examples/126-v-model-selection-list/src/components/VacationPicker.vue
+++ examples/126-v-model-selection-list/src/components/VacationPicker.vue
@@ -1,105 +1,64 @@
 <template>
-    <div>
-        <div class="row">
-            <div class="col-6">
-                <h1>{{ title }}</h1>
-                <ul class="list-group">
-                    <li v-for="(country, index) in data.countries"
-                        @click="selectCountry(index)"
-                        :key="country.id"
-                        class="list-group-item">
+  <h1>{{ header }}</h1>
+  <div class="row">
+    <div class="col-6">
+      <ul class="list-group">
+        <li
+            class="list-group-item"
+            v-for="country in data.countries"
+            :key="country.id">
                 <span :id="country.id"
                       :title="country.details">
-                    {{ country.id}} -
-                    {{ country.name }}
+                    {{ country.id }} - {{ country.name }}
                 </span>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-6">
-              <h2>Countries cheaper than</h2>
-              <label>
-              <select class="form-control-lg"
-                      v-model="selectedCost"
-                      @change="filterCountries"
-              >
-                <option v-for="(cost, index) in costs"
-                        :key="index"
-                        :value="cost">
-                  {{ cost }}
-                </option>
-              </select>
-              </label>
-              <ul class="list-group">
-                <li v-for="(country, index) in filteredCountries"
-                    :key="index"
-                    class="list-group-item">
-                  {{ country.name }} (EUR: {{ country.cost}})
-                </li>
-              </ul>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <hr>
-            </div>
-        </div>
+        </li>
+      </ul>
     </div>
+    <div class="col-6">
+      <!--  *********************-->
+      <!--  Creating a list of new countries via v-model-->
+      <!--  *********************-->
+      <h2>Countries cheaper than</h2>
+      <label>
+        <select class="form-control-lg"
+                v-model="selectedCost"
+                @change="filterCountries"
+        >
+          <option v-for="(cost, index) in costs"
+                  :key="index"
+                  :value="cost">
+            {{ cost }}
+          </option>
+        </select>
+      </label>
+      <ul class="list-group">
+        <li v-for="(country, index) in filteredCountries"
+            :key="index"
+            class="list-group-item">
+          {{ country.name }} (EUR: {{ country.cost }})
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
-<script>
-    import data from '../data/data';
-    import mixins from '../mixins/mixins';
+<script setup>
+import {ref} from "vue";
+// import the country countryData
+import countryData from '@/data/CountryData';
 
-    export default {
-        name: "VacationPicker",
-        data() {
-            return {
-                data,
-                title: 'Vue Vacation Picker',
-                selectedCountryIndex: 0,
-                costs: [1000, 2000, 3000, 4000, 5000, 6000],
-                selectedCost: 1000,
-                filteredCountries: []
-            }
-        },
-        // Code, mixed in to this component
-        mixins:[mixins],
-        // lifecycle hook
-        created(){
-            console.log('Vacation Picker is created...');
-        },
-        methods: {
-            selectCountry(index) {
-                this.selectedCountryIndex = index;
-            },
-            filterCountries(){
-                this.filteredCountries =
-                    this.data.countries.filter(country => country.cost < this.selectedCost)
-            }
-        },
-        computed: {
-            selectedCountry() {
-                return {
-                    // longhand notation
-                    // id: this.data.countries[this.selectedCountryIndex].id,
-                    // name: this.data.countries[this.selectedCountryIndex].name,
-                    // capital: this.data.countries[this.selectedCountryIndex].capital,
-                    // cost: this.data.countries[this.selectedCountryIndex].cost,
-                    // details: this.data.countries[this.selectedCountryIndex].details,
-                    // img: this.data.countries[this.selectedCountryIndex].img
+// create variables
+const header = 'Pick your next vacation';
+const data = countryData;
+const costs = ref([1000, 2000, 3000, 4000, 5000, 6000]);
+const selectedCost = ref(1000);
+const filteredCountries = ref([])
 
-                    // shorthand notation
-                    ...this.data.countries[this.selectedCountryIndex]
-                }
-            },
-            isExpensive(){
-                return this.data.countries[this.selectedCountryIndex].cost > 4000;
-            }
-        }
-    }
+// Method to calculate a new array, holding the correct countries.
+const filterCountries = () => {
+  filteredCountries.value = data.countries.filter(country => country.cost < selectedCost.value);
+}
+
+// Workshop: rewrite the function above to a computed value!
+
 </script>
-
-<style scoped>
-
-</style>
