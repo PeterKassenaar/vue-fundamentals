392a16ea0fed231dc6349cc02ff231ad9097cf99
PeterKassenaar <info@kassenaar.com>
2024-12-13 14:05:18 +0100

    feat: update example 200-props

--- examples/200-props/src/App.vue
+++ examples/200-props/src/App.vue
@@ -1,16 +1,9 @@
-<template>
-    <div id="app" class="container">
-        <VacationPicker />
-    </div>
-</template>
-
-<script>
-	import VacationPicker from "./components/VacationPicker";
-
-	export default {
-		name: 'app',
-		components: {
-			VacationPicker
-		}
-	}
+<script setup>
+import VacationPicker from "@/components/VacationPicker.vue";
 </script>
+
+<template>
+  <div id="app" class="container">
+    <VacationPicker/>
+  </div>
+</template>
--- examples/200-props/src/components/CountryDetail.vue
+++ examples/200-props/src/components/CountryDetail.vue
@@ -1,67 +1,47 @@
 <template>
-  <div>
-    <h2>{{ country.name }}</h2>
-    <ul class="list-group">
-      <li class="list-group-item">{{ country.id }}</li>
-      <li class="list-group-item">{{ country.name }}</li>
-      <li class="list-group-item">{{ country.capital }}</li>
-      <li class="list-group-item">
-        <img :src="getImgUrl(country.img)"
-             :alt="country.img"
-             class="img-fluid">
-      </li>
-      <li class="list-group-item">{{ country.details }}</li>
-      <li class="list-group-item" v-if="isExpensive">
-        <span class="badge badge-danger badge-pill">Expensive!</span>
-      </li>
-      <li class="list-group-item" v-if="isOnSale">
-        <span class="badge badge-warning badge-pill">On Sale!</span>
-      </li>
-    </ul>
-  </div>
+  <h2>{{ country.name }}</h2>
+  <ul class="list-group">
+    <li class="list-group-item">{{ country.id }}</li>
+    <li class="list-group-item">{{ country.name }}</li>
+    <li class="list-group-item">{{ country.capital }}</li>
+    <li class="list-group-item">
+      <img :src="imgUrl"
+           :alt="country.img"
+           class="img-fluid">
+    </li>
+    <li class="list-group-item">{{ country.details }}</li>
+    <li class="list-group-item" v-if="isExpensive">
+      <span class="badge bg-danger badge-pill">Expensive!</span>
+    </li>
+    <li class="list-group-item" v-if="isOnSale">
+      <span class="badge bg-warning badge-pill">On Sale!</span>
+    </li>
+  </ul>
 </template>
 
-<script>
-export default {
-  name    : "CountryDetail",
-  // Props are like a public 'API' for the component
-  // Option 1. Props as a named array (deprecated in Vue 3):
-  // props   : ['country'],
-  // Option 2. Extended props: as object notation. Extra options:
-  props   : {
-    country: {
-      type    : Object,
-      required: false,
-      default() {
-        return {
-          id     : -1,
-          name   : 'Unknown',
-          capital: 'Unknown',
-          img    : '',
-          details: '',
-          cost   : -1
-        }
-      }
-    }
-  },
-  beforeUpdate() {
-    console.log('Component updated! ==> ', this.country.name);
-  },
-  methods : {
-    getImgUrl(img) {
-      if (img) {
-        console.log(img);
-        return require('../assets/countries/' + img);
-      }
-    }
-  },
-  computed: {
-    isExpensive() {
-      return this.country.cost > 4000;
-    },
-    isOnSale() {
-      return this.country.cost < 1000;
-    }
-  }
-}
+
+<script setup>
+// We tell the component that it receives a 'country' property.
+// Also, we assign it to the local variable 'country', via ES6-destructuring.
+// The local country variable is reactive.
+import {computed} from "vue";
+
+const {country} = defineProps({
+  country : Object
+})
+
+// Automatically calculate if a destination is expensive
+const isExpensive = computed(() => {
+  return country.cost > 4000;
+});
+
+// Automatically calculate if a destination is on Sale
+const isOnSale = computed(() => {
+  return country.cost < 1000;
+});
+
+// A computed property that returns the URL to the image for currentCountry.
+const imgUrl = computed(() => {
+  return new URL(`/src/assets/countries/${country.img}`, import.meta.url).href;
+})
 </script>
