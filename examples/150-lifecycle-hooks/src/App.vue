5b674c79078182d02493a54fe0b665597c1d3a84
PeterKassenaar <info@kassenaar.com>
2024-12-13 11:11:36 +0100

    feat: update example 150-lifecycle-hooks

--- examples/150-lifecycle-hooks/src/App.vue
+++ examples/150-lifecycle-hooks/src/App.vue
@@ -1,26 +1,9 @@
-<template>
-    <div id="app" class="container">
-        <h1>Using lifecycle hooks</h1>
-        <VacationList />
-    </div>
-</template>
-
-<script>
-
-	import VacationList from "./components/VacationList";
-
-	export default {
-		name: 'app',
-		components: {
-			VacationList
-		}
-	}
+<script setup>
+import VacationPicker from "@/components/VacationPicker.vue";
 </script>
 
-<style>
-    /*This is a global style, because there is no 'scoped' keyword on the <style> tag */
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        color: #2c3e50;
-    }
-</style>
+<template>
+  <div id="app" class="container">
+    <VacationPicker />
+  </div>
+</template>
