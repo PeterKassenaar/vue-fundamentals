d3e930ad750df907d18d38b9f058318bded75d52
PeterKassenaar <info@kassenaar.com>
2025-09-05 14:22:41 +0200

    feat: update composable functions on ApiComponent.vue

--- examples/340-http-composable/src/composables/useCountries.js
+++ examples/340-http-composable/src/composables/useCountries.js
@@ -1,25 +1,26 @@
 // composables/useCountries.js
-import { ref } from "vue";
+import {ref} from "vue";
 import axios from "axios";
 
 export function useCountries() {
-  const countries = ref([]);
-  const errors = ref([]);
+    const countries = ref([]);
+    const errors = ref([]);
 
-  const fetchCountries = async () => {
-    const url = "https://restcountries.com/v3.1/all";
-    try {
-      const response = await axios.get(url);
-      countries.value = response.data;
-    } catch (error) {
-      errors.value.push(error);
-    }
-  };
+    const fetchCountries = async () => {
+        const fields = 'name,capital,population,flags'; // fields to retrieve from the API, see https://restcountries.com/v3.1/all
+        const url = `https://restcountries.com/v3.1/all?fields=${fields}`
+        try {
+            const response = await axios.get(url);
+            countries.value = response.data;
+        } catch (error) {
+            errors.value.push(error);
+        }
+    };
 
-  // Return the reactive data and the method
-  return {
-    countries,
-    errors,
-    fetchCountries,
-  };
+    // Return the reactive data and the method
+    return {
+        countries,
+        errors,
+        fetchCountries,
+    };
 }
